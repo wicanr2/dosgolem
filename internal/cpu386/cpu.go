5399ce8f99447eedaa7c6cd3ce17d87304e06d6c
Chun-Yu Wang <wicanr2@gmail.com>
2026-09-06 15:16:12 +0800

    feat: 初始化 DOS open handle

--- internal/cpu386/cpu.go
+++ internal/cpu386/cpu.go
@@ -1676,6 +1676,26 @@ func (c *CPU) Step() error {
 			}
 			break
 		}
+		if modrm>>6 == 1 && (modrm>>3)&7 == 0 && modrm&7 != ESP {
+			delta, e := c.fetch8()
+			if e != nil {
+				return fail(e.Error())
+			}
+			value, e := c.fetch32()
+			if e != nil {
+				return fail(e.Error())
+			}
+			base := modrm & 7
+			addr := uint32(int64(c.R[base]) + int64(int8(delta)))
+			segment := SegDS
+			if base == EBP {
+				segment = SegSS
+			}
+			if !c.writeSegment32(c.Seg[segment], addr, value) {
+				return fail(fmt.Sprintf("MOV immediate base+disp8 write %04X:%08X 未處理", c.Seg[segment], addr))
+			}
+			break
+		}
 		if modrm>>6 == 2 && (modrm>>3)&7 == 0 && modrm&7 != ESP {
 			delta, e := c.fetch32()
 			if e != nil {
